78fe62d43f370c227f4f7218699f1a8f31239736
HaoLiangPao <hflsforeverhao@gmail.com>
2020-02-17 17:46:03 -0500

    HCL and H2SO4 are successfully added to the database, however there are some other materials with different information available, some html structures should be changed.

--- Src/pubChemSearch.py
+++ Src/pubChemSearch.py
@@ -1,10 +1,14 @@
+import logging
+import traceback
+import time
+
+import pymongo
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs4
-import time
-import requests
 
 
 # # Set up for webpage connection
@@ -33,121 +37,117 @@ def xpath_search(driver, url, xpath):
     try:
         element = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
         return element
-    except:
-        return None
+    except Exception as e:
+        logging.error(traceback.format_exc())
 
 
 def get_attributes(browser, url, xpath):
-    element = xpath_search(browser, url, xpath)[0]
-    if element.text.find("\n"):
-        attribute = element.text[:element.text.find("\n")]
-    else:
-        attribute = element.text
-    return attribute
+    try:
+        element = xpath_search(browser, url, xpath)[0]
+        if element.text.find("\n"):
+            attribute = element.text[:element.text.find("\n")]
+        else:
+            attribute = element.text
+        return attribute
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        print("The attribute does not existed.")
+        return None
 
 
-def chemicalList(identifier, chrome):
-    chrome = webdriver.Chrome("../Drivers/chromedriver")
-    urlBase = "https://pubchem.ncbi.nlm.nih.gov/#query="
-    chrome.get(urlBase + identifier.replace(" ", "%20"))
+def chemical_list(browser, url):
     # these are absolute Xpath, may need to change if the layout of the website changes
-    suggestedXpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[" \
-                     "2]/div[1]/a[1]/span[1]/span[1] "
-    firstXpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/ul[" \
-                 "1]/li[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]/span[1]/span[1] "
-    element = xpath_search(chrome, urlFinal, suggestedXpath)
+    suggested_xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[" \
+                      "2]/div[1]/a[1]/span[1]/span[1] "
+    first_xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/ul[" \
+                  "1]/li[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]/span[1]/span[1] "
+    web_element = xpath_search(browser, url, suggested_xpath)
     # if given identifier does not have a suggested compound by PubChem
-    if element is None:
-        element = xpath_search(chrome, urlFinal, firstXpath)
-    soup = bs4(chrome.page_source, 'html.parser')
-    href = soup.findAll('a', {'class': 'capitalized'})[0]['href']
-    return href
-
-
-def getCompound(chrome, href, database):
-    chrome.get(href)
-
-    soup = bs4(chrome.page_source, "html.parser")  # get the html file from the chemical page
-    MW = soup.find('th', )  # molecular weight
-    syno = ""  # synonyms
-    MF = ""  # molecular formula
+    if web_element is None:
+        web_element = xpath_search(browser, url, first_xpath)
+    soup = bs4(browser.page_source, 'html.parser')
+    chemical_url = soup.findAll('a', {'class': 'capitalized'})[0]['href'] + "#datasheet=LCSS"
+    return chemical_url
 
 
 # pubChem search, which will open a chrome browser,
 # enter chemical identifiers into the search bar and click search
-def searchPubChem(identifier, chrome, chemical):
-    chrome.set_page_load_timeout(10)
+def searchPubChem(browser, chemical):
+    browser.set_page_load_timeout(10)
     # PubChem URL
-    chrome.get("https://pubchem.ncbi.nlm.nih.gov/")
+    browser.get("https://pubchem.ncbi.nlm.nih.gov/")
     search_xpath = "//input[starts-with(@id,'search')]"
     button_xpath = "//button[@class='button width-2em height-2em lh-1']"
     # find the input searching bar, enter the chemical name to be searched
-    chrome.find_element_by_xpath(search_xpath).send_keys(chemical)
+    browser.find_element_by_xpath(search_xpath).send_keys(chemical)
     print("Chemical entered")
     # find the search button, click and go to the detailed page
-    chrome.find_element_by_xpath(button_xpath).click()
+    browser.find_element_by_xpath(button_xpath).click()
     print("button clicked")
     time.sleep(4)
-    chrome.quit()
+    browser.quit()
 
 
-chrome = webdriver.Chrome("../Drivers/chromedriver")
-identifier = "HCL"
-urlBase = "https://pubchem.ncbi.nlm.nih.gov/#query="
-# 1. get the url for the specific compound
-# handel the space between the words in the identifier
-urlFinal = urlBase + identifier.replace(" ", "%20")
-suggestedXpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[" \
-                 "2]/div[1]/a[1]/span[1]/span[1] "
-firstXpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/ul[" \
-             "1]/li[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]/span[1]/span[1] "
-element = xpath_search(chrome, urlFinal, suggestedXpath)
-# if given identifier does not have a suggested compound by PubChem
-if element is None:
-    element = xpath_search(chrome, urlFinal, firstXpath)
-soup = bs4(chrome.page_source, 'html.parser')
-# directly go to Laboratory safety data sheet page
-href = soup.findAll('a', {'class': 'capitalized'})[0]['href'] + "#datasheet=LCSS"
-# 2. get the corresponding information and store them into the database
-MW_xpath = "//p[contains(text(),'g/mol')]"
-MW = get_attributes(chrome, href, MW_xpath)  # Molecular Weight
-ordor_xpath = "//section[@id='Odor']//div[@class='section-content-item']"
-ordor = get_attributes(chrome, href, ordor_xpath)  # Ordor
-BP_xpath = "//section[@id='Boiling-Point']//div[@class='section-content']//div[2]"
-BP = get_attributes(chrome, href, BP_xpath)  # boiling point
-MP_xpath = "//section[@id='Melting-Point']//div[@class='section-content']//div[2]"
-MP = get_attributes(chrome, href, MP_xpath)  # melting point
-solu_xpath = "//section[@id='Solubility']//div[@class='section-content']//div[2]"
-solu = get_attributes(chrome, href, solu_xpath)  # solubility
-dens_xpath = "//section[@id='Density']//div[@class='section-content']//div[2]"
-dens = get_attributes(dens_xpath)  # density
+def generate_chemical_profile(identifier):
+    # 1. get the url for the specific compound
+    # handel the space between the words in the identifier
+    chemical = {}
+    chrome = webdriver.Chrome("../Drivers/chromedriver")
+    chemical["identifier"] = identifier
+    urlBase = "https://pubchem.ncbi.nlm.nih.gov/#query="
+    urlFinal = urlBase + identifier.replace(" ", "%20")
+    href = chemical_list(chrome, urlFinal)  # get the webpage which contains the information about given chemical
+    # 2. get the corresponding information and store them into the database
+    # 2.1 get physical properties
+    MW_xpath = "//p[contains(text(),'g/mol')]"
+    chemical["MW"] = get_attributes(chrome, href, MW_xpath)  # Molecular Weight
+    ordor_xpath = "//section[@id='Odor']//div[@class='section-content-item']"
+    chemical["ordor"] = get_attributes(chrome, href, ordor_xpath)  # Ordor
+    BP_xpath = "//section[@id='Boiling-Point']//div[@class='section-content']//div[2]"
+    chemical["BP"] = get_attributes(chrome, href, BP_xpath)  # boiling point
+    MP_xpath = "//section[@id='Melting-Point']//div[@class='section-content']//div[2]"
+    chemical["MP"] = get_attributes(chrome, href, MP_xpath)  # melting point
+    solu_xpath = "//section[@id='Solubility']//div[@class='section-content']//div[2]"
+    chemical["solubility"] = get_attributes(chrome, href, solu_xpath)  # solubility
+    dens_xpath = "//section[@id='Density']//div[@class='section-content']//div[2]"
+    chemical["density"] = get_attributes(chrome, href, dens_xpath)  # density
+    # physical = ", ".join((ordor, BP, MP, solu, dens))
+    # print("Physical Properties:")
 
-print(ordor)
-print(BP)
+    # 2.2 get hazards properties
+    NFPA_pig_xpath = "//section[@id='NFPA-Hazard-Classification']//img[@class='icon']"
+    element = xpath_search(chrome, href, NFPA_pig_xpath)[0]
+    chemical["NFPA_pig"] = element.get_attribute("src")  # the src for the image of NFPA pigment
+    health_xpath = "//section[@id='Flammability-and-Explosivity']//tr[2]"
+    chemical["health_hazards"] = get_attributes(chrome, href, health_xpath)
+    fire_xpath = "//section[@id='Flammability-and-Explosivity']//tr[3]"
+    chemical["fire_hazards"] = get_attributes(chrome, href, fire_xpath)
+    inst_xpath = "//section[@id='Flammability-and-Explosivity']//tr[4]"
+    chemical["instability_hazards"] = get_attributes(chrome, href, inst_xpath)
+    # NFPA = health + "\n" + fire + "\n" + inst
+    # print("NFPA Hazards:")
+    # print(NFPA)
+
+    # 2.3 get first aids properties
+    inhal_xpath = "//section[@id='Inhalation-First-Aid']//div[@class='section-content-item']"
+    chemical["inhalation_first_aid"] = get_attributes(chrome, href, inhal_xpath)
+    skin_xpath = "//section[@id='Skin-First-Aid']//div[@class='section-content-item']"
+    chemical["skin_first_aid"] = get_attributes(chrome, href, skin_xpath)
+    eye_xpath = "//section[@id='Eye-First-Aid']//div[@class='section-content-item']"
+    chemical["eye_first_aid"] = get_attributes(chrome, href, eye_xpath)
+    inges_xpath = "//section[@id='Ingestion-First-Aid']//div[@class='section-content-item']"
+    chemical["ingestion_first_aid"] = get_attributes(chrome, href, inges_xpath)
+    print(chemical)
+
+    # 3. store the dictionary into the MongoDB
+    myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+    myDb = myClient["my_MSDS"]
+    Chemicals = myDb["Chemicals"]
+    if Chemicals.find(chemical).count() == 0:  # no replicate data captured
+        Chemicals.insert_one(chemical)
+    chrome.quit()  # terminate the browser as the search ends
 
 
-
-
-# # put all the information together
-# # physical properties
-# color = ""
-# ordor = ""
-# BP = 0  # boiling point
-# MP = 0  # melting point
-# solu = 0  # solubility
-# dens = 0  # density
-# physical = ", ".join((color, ordor, BP, MP, solu, dens))
-#
-# # Potential Hazards
-# NFPA_pig = "link"  # the pigment of NFPA hazards
-# health = ""  # NFPA health rating
-# fire = ""  # NFPA fire rating
-# instability = ""  # NFPA instability rating
-# NFPA = "health:" + health + "\n" + "fire:" + fire + "\n" + "instability:" + instability
-#
-# # First Aids
-# inhal = ""  # inhalation aids
-# skin = ""  # skin first aids
-# eye = ""  # eye first aids
-# inje = ""  # ingestion first aids
-# firstAids = "inhalation:" + inhal + "\n" + "skin:" + skin + "\n" + "eye:" + eye + "\n" + "injection:" + inhal
+if __name__ == "__main__":
+    identifier = input("Please enter the chemicals you are looking for: ")
+    generate_chemical_profile(identifier)
